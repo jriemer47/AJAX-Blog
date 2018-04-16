89e1e7365ec7e135e207b6d1af560bcfa4cd35d1
Jonathan Riemer <jriemer@Jonathans-MacBook-Pro-2.local>
2018-04-15 20:41:09 -0700

    connected getAll to front and back end

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
+const baseURL = "http://localhost:3000/blogs"
+
 document.addEventListener("DOMContentLoaded", event => {
   console.log("yo dog")
+  axios.get(`${baseURL}`).then(result => {
+    const blogs = result.data.blogs
+    blogs.forEach(blog => {
+      $("#sidebar").append(
+        `<ul class="list-group">
+          <a href="#/blogs/${blog.id}"
+            class="list-group-item list-group-item-action">
+              ${blog.title}
+          </a>
+        </ul>`
+      )
+    })
+  })
 })
 
-document.getElementById("create-post").addEventListener("click", function() {
-  console.log("append is this working")
-  $("#view").append(`<form id="post-form" action="/posts/">
-    <div class="form-group">
-      <label for="title">Title</label>
-      <input type="text" class="form-control" id="title" value="">
-    </div>
-    <div class="form-group">
-      <label for="title">Content</label>
-      <textarea type="text" rows="6" class="form-control" id="content"></textarea>
-    </div>
-    <button type="submit" class="btn btn-info btn-large">Create New Post</button>
-  </form>`)
+let newBlogForm = document.getElementById("create-post")
+newBlogForm.addEventListener("click", event => {
+  $("#view").append(
+    `<form id="post-form" action="/posts/">
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" id="title" value="">
+      </div>
+      <div class="form-group">
+        <label for="title">Content</label>
+        <textarea type="text" rows="6" class="form-control" id="content"></textarea>
+      </div>
+      <button type="submit" class="btn btn-info btn-large">Create New Post</button>
+    </form>`
+  )
 })
